6d92456ab8d2c80b68c5b661d98b9a0414db20d2
Mario Ivan Gonzalez Barillas <li-mgonzalez@hotmail.com>
2022-02-24 20:10:49 -0600

    010 - Debugging y comentarios

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-Primer cambio 
+<?php
+
+// echo "Hola, mamá. Estoy programando con PHPCito \n";
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+// echo "No, no. Yo me llamo " . $nombre . " " . $apellido . "\n";
+
+// echo "No, no. Yo me llamo $nombre $apellido \n";
+
+// echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+$personas = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+];
+
+// var_dump( $personas );
+// print_r( $personas );
+
+// Esto es un comentario
+# Esto es otro comentario
+
+/*
+
+Cualquieeeeeer cosa,
+abajo puedo seguir escribiendo más cosas
+UwU
+
+*/
+
+echo "\n";
